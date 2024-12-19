38e3fee6fd2a7c096eedae076f5535baca813be2
powdhar <33878232+powdhar@users.noreply.github.com>
2024-12-19 14:27:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+const quotes = 
+[
+    "The weather is perfect. The gods are shining on us.",
+    "Because running fast is more fun than running slow.",
+    "I think he had a wake-up call. It's a different kind of race, and I think maybe he didn't take it quite as seriously as he might have, but you can bet he learned a lot of lessons.",
+    "Hills are speedwork in disguise.",
+    "The potential elite runner must realize that hard means hard, easy means easy and they must patiently seek out what combinations work for them. They have to learn to be persistent and patient with their training and racing.",
+    "Intervals and other types of speed work are essential to improve running speed.",
+    "You don't run 26 miles at five minutes a mile on good looks and a secret recipe.",
+    "Yes, winning the gold medal was undoubtedly the biggest day of my career - mostly because I won the way I had prepared to run it. It was a totally satisfying experience.",
+    "A good athlete always mentally replays a competition over and over, even in victory, to see what might be done to improve the performance the next time.",
+    "Again, racing for me was about energy management.",
+    "As I've been able to once again gain the benefits of speed work, I'm enjoying my running more and more.",
+    "Being in school is the best place to be if you are an athlete because you can structure your own time.",
+    "Experience has taught me how important it is to just keep going, focusing on running fast and relaxed. Eventually it passes and the flow returns. It's part of racing.",
+    "For the novice runner, I'd say to give yourself at least 2 months of consistently running several times a week at a conversational pace before deciding whether you want to stick with it. Consistency is the most important aspect of training at this point.",
+    "I admire runners older than I - they are now my heroes. I want to be like them as I grow older.",
+    "I also held several masters running titles.",
+    "I plan to be running as long as I can and have no plans to stop.",
+    "I started in law school in '71 and graduated in '74. So I was training for the Olympics, running or averaging around 20 miles a day and going to law school full time.",
+    "I think it is that parents just don't kick their kids out the door as much as they used to. I think the demise of sandlot sports has had a lot to do with it.",
+    "I think the secret of my light, quick, foot strike is related to the fact that I have fragile feet.",
+    "I was actually going to law school in 1972.",
+    "In other words we have marketed our way into this health crisis.",
+    "It is just called Continuing Legal Education. You can go to lectures, you can even listen to tapes on airplanes - they want you to stay current. So you do have to stay current to maintain your license even if you are not practicing.",
+    "It is not the time spent with the child at their activity that is going to produce the highest level athlete. It is in supporting the child in an organized activity - and Bill alluded to this - so the child can find what they truly like to do and let them go.",
+    "My goal is to break three hours in a marathon.",
+    "Really, I think that going out and playing with your friends is kind of becoming a lost art, with the kids in the neighborhood.",
+    "Right now, after having had back surgery, I am finally back to running again.",
+    "Running with others can help get you out when you might otherwise blow it off.",
+    "So, in a way I was hedging and saying that if the Olympic stuff doesn't work out at least I can be a lawyer.",
+    "The irony of that is, what makes it kind of ironic, is when you do become successful as a professional athlete in particular, a lot of the young children who are emulating these stars do have a different perspective.",
+    "The marathon is all about energy management. I had planned to run it like a track race with strategic surges to blow up my competitors by putting them into oxygen debt, so that is the way I prepared.",
+    "There's obviously some validity to it. But I think it also points out that you obviously can do it on your own because people have been doing it long before they had the stuff.",
+    "To put it another way, Michael Jordan was a gym rat.",
+    "What you did was live on very little. I think all of us that were competing - Bill is the same way - you don't need much to live.",
+    "When I am totally race fit, I don't worry about breathing or technique - they take care of themselves.",
+    "When you are caring about your children perhaps you always have to remember at what point you can become over involved because of something you need rather than something the child needs.",
+    "To be a consistent winner means preparing not just one day, one month or even one year - but for a lifetime.",
+    "If you want to win a race you have to go a little berserk.",
+    "I like rain, actually.",
+    "The marathon can humble you.",
+    "My whole feeling in terms of racing is that you have to be very bold. You sometimes have to be aggressive and gamble.",
+    "Anybody can be a runner... We were meant to move. We were meant to run. It's the easiest sport.",
+    "I believe in gradual experimentation with running shoes.",
+    "Age 50 is a lot tougher than 40s.",
+    "Being a runner means you are now 'free' to win and lose and live life to its fullest.",
+    "Every race is totally different.",
+    "Hills are terrific for running.",
+    "I always say if the marathon is a part-time interest, you will only get part-time results.",
+    "I believe in keeping running simple and, in regard to shoes, that would mean no gimmicks, unnecessary cushioning, etc.",
+    "I believe you'll develop speed via strength work which includes hill running, either repeats, or running hilly courses as the Kenyans do on a steady basis.",
+    "I ran my fastest marathon in the rain.",
+    "I want to run until I can't run.",
+    "My biggest weakness as a endurance athlete has been in not drinking enough water after training, thereby racing sometimes while dehydrated.",
+    "My guess is many top athletes, distance runners included, use performance-enhancing drugs, enough so that the problem must be tackled.",
+    "The starting line of the New York Marathon is kind of like a giant time bomb behind you about to go off. It is the most spectacular start in sport.",
+    "You cannot always run at your best.",
+    "Only the disciplined ones in life are free. If you are undisciplined, you are a slave to your moods and your passions.",
+    "If you believe in something and put it in your mind and heart, it can be realised.",
+    "No human is limited.",
+    "Athletics is not so much about the legs. It's about the heart and mind.",
+    "I'm confident to say that if you want to grow in a profession, consistency is the key... I'm strict about my work goals and training.",
+    "The best time to plant a tree was 25 years ago. The second-best time to plant a tree is today.",
+    "To me, running is life. I run to live longer and enjoy a healthier life.",
+    "Where the marathon starts is after 30 kilometers. That's where you feel pain everywhere in your body. The muscles are really aching, and only the most prepared and well-organized athlete is going to do well after that.",
+    "To win is not important. To be successful is not even important. How to plan and prepare is crucial. When you plan very well and prepare very well, then success can come on the way. Then winning can come on your way.",
+    "If you are humble, your concentration will be very high. That's the way to go in sports.",
+    "I wake up at 5am, by 6am I'm on the way to training. I come back and relax, have lunch, take a little nap, then train again at 4pm for an evening run. Then relax, dinner and bed at 9pm until the next day.",
+    "Thinking positively and confidently changed my life. If you stay consistent and work hard, you can be successful for a long time.",
+    "Long runs improve your endurance but shorter intervals improve your speed. They are mutually beneficial for a distance runner. If you do 5k runs it will help your marathon.",
+    "I am running to make history, to show that no human is limited. It's not about money, it's about showing a generation of people that there are no limits.",
+    "If you have that belief - pure belief in your heart - that you want to be successful then you can talk to your mind and your mind will control you to be successful.",
+    "Once in a while you can run very fast, but you cannot always go to the maximum.",
+    "I always say: a run in the morning is like eating a fruit a day - it chases he doctor away. It is good for your mind.",
+    "I have kept a training diary to record my training plans and my feelings and emotions for a long time.",
+    "I always tell people that this is a really simple deal: Work hard. If you work hard, follow what's required and set your priorities right, then you can really perform without taking shortcuts. If you're taking shortcuts, you can't be free.",
+    "If you concentrate on the rear mirror, you'll crash and cause an accident.",
+    "To run a big marathon and win takes five months. When I'm on the starting line, my mind starts reviewing what I have been doing the last five months. I believe in my training, and I treat myself as the best one standing on that line.",
+    "I started running because my neighbour, Patrick Sang, was an athlete and I wanted to be just like him. Patrick came from the same village as I do and my mother used to be his teacher. I was so inspired by his success.",
+    "Others will absolutely be able to break 2:00, too, as long as they believe in themselves.",
+    "I am just going to try to run my personal best. If it comes as a world record, I would appreciate it. But I would treat it as a personal best.",
+    "I want to show the world that you can go beyond your thoughts, you can break more than you think you can break.",
+    "I got into running when I was being mentored by my coach. I tried it and developed a passion for it.",
+    "I think 1:59 is something a human being can run. Or 1:58. Who knows?! If someone trains very well and thinks in a good way, then they can do anything.",
+    "I have only ever done athletics. I was only average at school and I have never done any other sport.",
+    "I enjoy the simplistic training and life in marathon. You run, eat, sleep, walk around - that's how life is. You don't get complicated. The moment you get complicated it distracts your mind.",
+    "I want to run with a relaxed mind.",
+    "I was not so lucky to grow up with toys as I grew up in a remote area of Kenya.",
+    "It is a long time now since I started running but I still remember running up and down hills and running to school as a kid. When I was young I would run for fun and I didn't know back then that this would be my career.",
+    "If you want to break through, your mind should be able to control your body. Your mind should be a part of your fitness.",
+    "I would say that Breaking2 was my best race because I was running against something no one had done before.",
+    "I'm not having any problems with my body so I don't need to supplement. Growing older you don't recover as fast, but all-in-all I'm doing well.",
+    "I've always found the secret is to stay fit and treat sport as a profession. I chose sport as a passion.",
+    "I really want to make a big mark in athletics, to be a legend and be a good example to kids.",
+    "You need to go near to the people and actually sensitize them about the sport - specifically running. If athletes can go near their fans, many people would develop a love for running.",
+    "One of my first races came over 10km in 2002. I won that race and it felt great. I would say that is when I first fell in love with running.",
+    "My mind is always free. My mind is flexible.",
+    "It's important to run clean so that you can inspire people, to give an example to the young generation.",
+    "I want to stay for a long time and leave a mark so the young generation can aspire to be at the top like me. I love the sport, and that's what drives me.",
+    "In the offseason I allow my body to recover, my mind to recover. I like to be with my family, to read books, and know what is going on in the world, to understand how people think.",
+    "Vienna is a city where they love sports.",
+    "It's like the first man to go to the moon, I will be the first man to run under two hours, this is crucial.",
+    "Mentality is where the success is. I will teach the kids how to think in a positive way.",
+    "If you train with fast athletes, it keeps you pushing. I know that good preparation is more important than everything else.",
+    "My first race was in October 2001 in Kapsabet, Kenya. It was a 10km road race. I was excited and I was happy to know I am good in running.",
+    "My first competition outside Kenya was at the 2002 world cross country championships in Dublin, Ireland. I finished fifth in the junior race that day but the thing I remember the most was that it was very cold."
+]
+
+const images = 
+    ['images/image001.jpg','images/image002.jpg', 'images/image003.jpg','images/image004.jpg','images/image005.jpg', 'images/image006.jpg', 'images/image007.jpg', 'images/image008.jpg']
+
+function showQuotes () {
+    let text = document.getElementById('text');
+    let selectQuote = Math.floor(Math.random() * quotes.length);
+    text.innerHTML = quotes[selectQuote];
+}
+
+function randomImage () {
+    let selectImage = Math.floor(Math.random() * images.length);
+    let image = document.getElementById('image');
+    image.src = images[selectImage];
+}
+
+function rotateImage () {
+    let numberOfDegs = Math.round(Math.random() * 100) / 25;
+    let bothDegs = numberOfDegs * Math.round(Math.random()) * 2 - 1
+    let string = 'rotate(' + bothDegs + 'deg)'
+    document.getElementById("image").style.transform = string;
+}
+
+showQuotes();
+randomImage();
+rotateImage();
+
+let time = 8000;
+
+window.setInterval(showQuotes,time);
+window.setInterval(rotateImage,time);
+window.setInterval(randomImage,time);
+
+ 
